4c74efe1317db81f6959e8a0323f119b4617475a
표룡희_2019203075 <vyfydgml10@gmail.com>
2022-07-17 09:33:08 +0000

    test1

--- src/project/Main.java
+++ src/project/Main.java
@@ -2,6 +2,7 @@ package project;
 
 public class Main {
     public static void main(String[] args) {
-      System.out.println("STUDY");
+      	System.out.println("STUDY");
+        System.out.println("HEllo");
     }
 }
